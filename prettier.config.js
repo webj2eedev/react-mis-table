13100fae3951af2615f02f8fd31a571ecb16220d
webj2ee <webj2ee@qq.com>
2023-11-03 16:59:34 +0800

    init

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 4,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+}
